49a136da81af60ebe09c6d20f6a1b7e731a5fcc0
Rhone06 <146122568+Rhone06@users.noreply.github.com>
2024-09-16 14:51:33 -0400

    Update guessNumber.cs

--- 01_gaming_exercises/02_guess_a_number/guessNumber.cs
+++ 01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -1,16 +1,18 @@
-using Sysytem;
+using System;
 class guessNumber {
- static void Main() {
+ static void Main(string[] args) {
     Random r = new Random(); 
-
+    playerGuess
     int val = r.Next(1, 50);
-    int guess = 0;
+    int guess;
+    int allowedTries = 15; 
+    int numberOfTries = 1;
     bool correct = false;
 
     Console.WriteLine(" I'm Thinking of a number between 1 & 50.\n");
 
     while (!correct)
-
+    {
     Console.Write("Guess: ");
     string input = Console.ReadLine();
 
@@ -20,11 +22,22 @@ class guessNumber {
         continue; 
     }
 
-    if (guess < val)
+    if (playerGuess < val)
     {
-        Console.WriteLine ("Not Quite, The number I'm thinking of is higher than")
+        Console.WriteLine ("Not Quite, The number I'm thinking of is Higher than that number.\n");
+    }
+    else if (playerGuess > val)
+    {
+        Console.WriteLine ("Not Quite, The number I'm thinking of is Lower than that number.\n");
+    }
+    else 
+    {
+        correct = true;
+        Console.WriteLine("You guessed right, you win!");
+        break;
     }
 
+    Console.WriteLine($"You have {allowedTries - numberOfTries} tries left. Please enter another number:\n");
+  }
  }
 }
- 
