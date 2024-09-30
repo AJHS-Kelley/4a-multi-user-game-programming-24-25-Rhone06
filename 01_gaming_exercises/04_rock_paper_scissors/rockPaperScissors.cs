488a7132df875c98670ebd7d49d6fd42181c5f59
Rhone06 <146122568+Rhone06@users.noreply.github.com>
2024-09-30 14:05:09 -0400

    Update rockPaperScissors.cs

--- 01_gaming_exercises/04_rock_paper_scissors/rockPaperScissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rockPaperScissors.cs
@@ -1,4 +1,5 @@
 using System;
+
 class rockPaperScissors {
   static void Main() {
   // PLAYER VARIABLES
@@ -16,8 +17,36 @@ class rockPaperScissors {
   Console.WriteLine($"Greetings {playerName}. Is this correct?\n");
   Console.WriteLine("Please type yes or no, then press ENTER.\n");
   string isCorrect = Console.ReadLine().ToLower();
-  
 
+  if (isCorrect == "yes")
+  {
+    Console.WriteLine($"Splendid! I'll call you {playerName}.\n");
+  }
+  else if (isCorrect == "no")
+  {
+    Console.WriteLine("Please type your preffered name and press ENTER. \n");
+    playerName = Console.ReadLine();
+  }
+  else
+  {
+    Console.WriteLine("Unable to determine your answer. Please try again. \n");
+  }
+
+  Console.WriteLine("""
+  +===========================================================+ 
+  |                                                           |
+  |     WELCOME TO THE ROCK, PAPER, SCISSORS CHALLENGE!       |
+  +===========================================================+
+  You will select from Rock, Paper, or Scissors. 
+  The CPU will select one of those choices at random. 
+  Then a winner will be determined using the following rules:
+  -- Rock Beats Scissors
+  -- Paper Beats Rock 
+  -- Scissors Beats Paper 
+
+  The winner will receive a point. 
+  The first player to score 5 points will be declared the winner.
+  """);
 
   }
 }
