12db7f80f7b3e0e3410b48f8a12a9c8dad44bfb7
Rhone06 <146122568+Rhone06@users.noreply.github.com>
2024-10-30 12:38:23 -0400

    .

---
+++ 00_computer_science_exercises/04_collections/template.cs
@@ -0,0 +1,27 @@
+//Collections example, Ja'Shaun Rhone, v0.0
+using System;
+using System.Linq;
+using System.Collections; 
+
+class 04_Collections {
+  static void Main() {
+
+  /*
+  In C# an collection is known as an ARRAY.
+  -- EACH ITEM IN THE ARRAY IS KNOWN AS AN ELEMENT.
+  -- NUMBER OF ELEMENTS IN THE ARRAY CANNOT BE CHANGED.
+  -- CONTENTS OF EACH ELEMENT CAN CHANGE.
+  -- ARRAYS ARE ORDERED, MEANING EACH ITEM HAS A FIXED POSITION.
+  -- POSITION OF EACH ELEMENT IS CALLED THE INDEX.
+  -- FIRST ELEMENT INDEX IS 0. 
+  */
+
+  //DECLARING AND DEFYING AN ARRAY. 
+  string[] breakfastFoods = {"Cereal", "eggs", "Toast", "Hash Brown", "Bagel"};
+  int[] testScore = {0, 25, 50, 75, 90};
+  double[] GPA = {0.27D, 1.99D, 4.02D, 0.0D, 3.25D};
+
+
+
+  } // DO NOT DELETE EVER, 3 SPACES INDENTED FROM THE LEFT
+} // DO NOT DELETE EVER, ALWAYS TOUCHING LEFT MARGIN
