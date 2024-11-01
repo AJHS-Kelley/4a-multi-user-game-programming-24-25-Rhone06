aed14ce8a187221ebac584951ba9d369b7df6517
Rhone06 <146122568+Rhone06@users.noreply.github.com>
2024-11-01 13:48:44 -0400

    Update template.cs

--- 00_computer_science_exercises/04_collections/template.cs
+++ 00_computer_science_exercises/04_collections/template.cs
@@ -18,17 +18,17 @@ class Collections {
 
   //DECLARING AND DEFYING AN ARRAY. 
   string[] breakfastFoods = {"Cereal", "eggs", "Toast", "Hash Brown", "Bagel"};
-  int[] testScore = {0, 25, 50, 75, 90};
+  int[] testScores = {0, 25, 50, 75, 90};
   double[] GPA = {0.27D, 1.99D, 4.02D, 0.0D, 3.25D};
-
+  
   // Print Array contents -- All Elements on Single Line
   Console.WriteLine("The elements of each array are:\n");
   Console.WriteLine("breakFastFoods: \n" + String.Join(", ", breakfastFoods));
   Console.WriteLine(); // Print an empty line to the screen.
 
-  // Print Array Elements on Seperate Lines
+  /* Print Array Elements on Seperate Lines
   Console.WriteLine("The elements of each array are:\n");
-  Console.WriteLine("testScore: \n" + String.Join("\n", testScore));
+  Console.WriteLine("testScores: \n" + String.Join("\n", testScores));
   Console.WriteLine(); // Print an empty line to the scree
 
   // How long is my array? DETERMINING ARRAY LENGTH 
@@ -37,10 +37,22 @@ class Collections {
 
   // Accessing Elments in the array with the index
   Console.WriteLine("The first element in breakFastFoods is: " + breakfastFoods[0]);
-  // PRIN THE 2nd ELEMENT OF THE testScores array. 
-  Console.WriteLine("The second element in testScores is: " + testScore [1]);
+  // PRIN THE 2nd ELEMENT OF THE  array. 
+  Console.WriteLine("The second element in  is: " + testScores [1]);
   //PRINT THE 3rd ELEMENT OF THE GPA array. 
   Console.WriteLine("The third element in GPA is: " + GPA [2]);
+  
+  // Shortcut to any item in an Array
+  //Console.WriteLine("The last element of breakFastFoods is: " + breakfastFoods[breakfastFoods.Length - 1]); 
+  //Console.WriteLine("The last element of testScores is: " + testScores[testScores.Length - 1]); 
 
+  // Changing an array
+  breakfastFoods[2] = "Burnt Toast";
+  testScores[3] = 80;
+  GPA[1] = 2.0D;
+  Console.WriteLine("The elements of each array are:\n");
+  Console.WriteLine("breakFastFoods: \n" + String.Join(", ", breakfastFoods));
+  Console.WriteLine(); // Print an empty line to the screen.
+  */
   } // DO NOT DELETE EVER, 3 SPACES INDENTED FROM THE LEFT
 } // DO NOT DELETE EVER, ALWAYS TOUCHING LEFT MARGIN
